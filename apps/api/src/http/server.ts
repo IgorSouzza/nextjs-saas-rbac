a05a30896f3c9dd2ccb1423546828bd3c7f72152
Igor Souza <igor.souza.webmaster@gmail.com>
2024-07-07 18:12:27 -0300

    feat: add authenticateWithPassword route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,5 @@
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
@@ -9,6 +10,7 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -33,8 +35,10 @@ app.register(fastifySwaggerUI, {
 })
 
 app.register(fastifyCors)
+app.register(fastifyJwt, { secret: 'my-secret' })
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
